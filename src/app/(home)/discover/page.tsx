16d96ae25797d2bbe55c6804cc654f347ca3c8fa
ecarry <lianshiliang93@gmail.com>
2025-01-02 16:46:00 +0800

    rebuild: discover page

--- src/app/(home)/discover/page.tsx
+++ src/app/(home)/discover/page.tsx
@@ -1,6 +1,6 @@
 import VectorCombined from "@/components/vector-combined";
 import MapWithPhotos from "./map-with-photos";
-import PhotosGrid from "./photos-grid";
+//import PhotosGrid from "./photos-grid";
 
 export const metadata = {
   title: "Discover",
@@ -10,22 +10,28 @@ export const metadata = {
 
 const DiscoverPage = () => {
   return (
-    <div className="flex w-full h-full">
-      {/* Left side - Fixed Map */}
-      <div className="w-1/2 h-[calc(100%-24px)] fixed left-3 top-3 bottom-3 rounded-xl overflow-hidden">
-        <div className="w-full h-full rounded-l-[18px] overflow-hidden relative">
-          <MapWithPhotos />
-          <div className="absolute right-0 bottom-0 z-10">
-            <VectorCombined title="Discover" position="bottom-right" />
-          </div>
-        </div>
-      </div>
-
-      {/* Right side - Photo Grid */}
-      <div className="w-1/2 h-full ml-[50%] pl-6">
-        <PhotosGrid />
+    <div className="w-full h-full rounded-[18px] overflow-hidden relative">
+      <MapWithPhotos />
+      <div className="absolute right-0 bottom-0 z-10">
+        <VectorCombined title="Discover" position="bottom-right" />
       </div>
     </div>
+    // <div className="flex w-full h-full">
+    //   {/* Left side - Fixed Map */}
+    //   <div className="w-1/2 h-[calc(100%-24px)] fixed left-3 top-3 bottom-3 rounded-xl overflow-hidden">
+    //     <div className="w-full h-full rounded-l-[18px] overflow-hidden relative">
+    //       <MapWithPhotos />
+    //       <div className="absolute right-0 bottom-0 z-10">
+    //         <VectorCombined title="Discover" position="bottom-right" />
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   {/* Right side - Photo Grid */}
+    //   <div className="w-1/2 h-full ml-[50%] pl-6">
+    //     <PhotosGrid />
+    //   </div>
+    // </div>
   );
 };
 
