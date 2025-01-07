7faa11f84075ba1cffc9cde7e89b4417d1c4a9eb
ecarry <lianshiliang93@gmail.com>
2025-01-07 17:28:45 +0800

    test: expandable card component

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import { ExpandableCard } from "@/components/expandable-card";
+
+const page = () => {
+  return (
+    <div className="size-full">
+      <ExpandableCard />
+    </div>
+  );
+};
+
+export default page;
