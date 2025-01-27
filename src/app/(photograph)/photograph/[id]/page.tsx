8f7de07b0c71bfa7abcb5b527e98eb3772740275
ecarry <lianshiliang93@gmail.com>
2025-01-27 15:26:39 +0800

    seo: metadata

--- src/app/(photograph)/photograph/[id]/page.tsx
+++ src/app/(photograph)/photograph/[id]/page.tsx
@@ -4,6 +4,24 @@ type Props = {
   params: Promise<{ id: string }>;
 };
 
+const getPhoto = async (id: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/photos/${id}`
+  );
+
+  return res.json();
+};
+
+export const generateMetadata = async ({ params }: Props) => {
+  const id = (await params).id;
+  const photo = await getPhoto(id);
+
+  return {
+    title: `${photo.data.title}`,
+    description: `${photo.data.description}`,
+  };
+};
+
 const page = async ({ params }: Props) => {
   const id = (await params).id;
 
