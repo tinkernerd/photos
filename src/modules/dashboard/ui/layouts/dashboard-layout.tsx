fb838f86c19984e821ffd8c5f24012a120943403
ecarry <lianshiliang93@gmail.com>
2025-02-17 12:55:59 +0800

    fix: scroll bar

--- src/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ src/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -9,9 +9,9 @@ export const DashboardLayout = ({
 }) => {
   return (
     <SidebarProvider>
-      <div className="flex h-screen w-full flex-col">
+      <div className="flex h-screen w-full flex-col overflow-hidden">
         <DashboardNavbar />
-        <div className="flex flex-1 pt-[4rem]">
+        <div className="flex flex-1 h-[calc(100vh-4rem)] mt-16">
           <DashboardSidebar />
           <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
