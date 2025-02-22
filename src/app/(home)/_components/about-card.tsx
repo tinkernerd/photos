4b9e335011452e8139849637bc6c38930df62117
Nicholas Stull <Nicholasp.stull@gmail.com>
2025-02-21 22:40:19 -0500

    changed about info and photo

--- src/app/(home)/_components/about-card.tsx
+++ src/app/(home)/_components/about-card.tsx
@@ -6,19 +6,19 @@ const AboutCard = () => {
       <div className="flex flex-col p-12 gap-[128px]">
         <h1 className="text-3xl">About</h1>
         <div className="flex flex-col gap-4 font-light">
-          <p>
-            With a focus on both candid moments and stunning landscapes, I
-            strive to evoke emotion and tell stories through my work. My
-            photography blends the rawness of everyday life with the artistry of
-            fine art, allowing viewers to connect with each image on a deeper
-            level.
-          </p>
+        <p>
+            Starting out flying drones as a hobby, I quickly became captivated by
+            the art of photography and videography. I decided to sell my drone and get a camera,
+            which gives me the ability to capture more than just what is seen in the
+            sky. I am completely self-taught, and I am always looking to learn and see
+            what new things I can find, create, and do. </p>
 
           <p>
             Whether I&apos;m exploring urban environments or venturing into
             nature, my goal is to highlight the extraordinary in the ordinary.
             Through my lens, I invite you to join me on this visual journey of
-            discovery and inspiration.
+            discovery and inspiration. I enjoy finding everything that God has created
+            through a diffrent lense.
           </p>
         </div>
       </div>
